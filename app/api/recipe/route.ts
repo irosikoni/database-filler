0c851fbe82186e97cb486e74f52c25f9a129b695
iza <irosikoni@gmail.com>
2023-08-29 00:18:32 +0200

    schema unique added, working on post recipe

--- app/api/recipe/route.ts
+++ app/api/recipe/route.ts
@@ -1,32 +1,45 @@
 import { prisma } from "../../lib/prisma";
 import { NextResponse } from "next/server";
 
-export async function postRecipe(request: Request) {
+export async function POST(request: Request) {
   try {
     const body = await request.json();
     const recipe = await prisma.recipe.upsert({
-      data: {
-        where: { title: body.title },
-        update: {},
-        create: {
-          title: body.title,
-          description: body.description,
-          ingredients: {
-            create: body.ingredients,
+      where: { title: body.title },
+      update: {},
+      create: {
+        title: body.title,
+        description: body.description,
+        ingredients: {
+          createMany: {
+            data: body.ingredients.map((ingredient) => ({
+              name: ingredient.name,
+              amount: ingredient.amount,
+              unit: ingredient.unit,
+              connect: {
+                id: ingredient.recipeId,
+                baseIngredientId: ingredient.baseIngredientId,
+              },
+              upsert: {
+                where: {},
+                update: {},
+                create: {},
+              },
+            })),
           },
-          preparation: body.preparation,
-          advises: body.advises ? body.advises : null,
-          image: body.image ? body.image : null,
-          category: {
-            connect: {
-              id: body.categoryId,
-            },
-            createdAt: new Date(),
-            lastUpdate: new Date(),
-            reviews: {
-              create: body.reviews,
-            },
+        },
+        preparation: body.preparation,
+        advises: body.advises ? body.advises : null,
+        image: body.image ? body.image : null,
+        category: {
+          connect: {
+            id: body.categoryId,
           },
+        },
+        createdAt: new Date(),
+        lastUpdate: new Date(),
+        reviews: {
+          create: body.reviews,
         },
       },
     });
