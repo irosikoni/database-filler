b7e7eb29df6d114bab88362036db752765638396
iza <irosikoni@gmail.com>
2023-09-07 01:33:45 +0200

    adding few inputs

--- app/page.tsx
+++ app/page.tsx
@@ -5,29 +5,46 @@ export default function Home() {
     <div className="flex flex-col items-center content-center justify-center p-24">
       <div className="flex flex-col items-center justify-center content-center p-10 m-10 border-black border-4">
         <form className="flex flex-col items-center justify-center p-10">
-          <div className="flex flex-col items-center justify-center">
-            <label className="text-2xl font-bold">Title</label>
-            <input></input>
+          <div>
+            <div className="flex flex-col items-center justify-center">
+              <label className="text-2xl font-bold">Title</label>
+              <input></input>
+            </div>
+            <div className="flex flex-col items-center justify-center">
+              <label className="text-2xl font-bold">Description</label>
+              <input></input>
+            </div>
+            <div className="flex flex-col items-center justify-center">
+              <label className="text-2xl font-bold">Preparation</label>
+              <input></input>
+            </div>
+            <div className="flex flex-col items-center justify-center">
+              <label className="text-2xl font-bold">Advises</label>
+              <input></input>
+            </div>
+            <div className="flex flex-col items-center justify-center">
+              <label className="text-2xl font-bold">Image</label>
+              <input></input>
+            </div>
+            <div className="flex flex-col items-center justify-center">
+              <label className="text-2xl font-bold">Category</label>
+              <input></input>
+            </div>
           </div>
-          <div className="flex flex-col items-center justify-center">
-            <label className="text-2xl font-bold">Description</label>
-            <input></input>
-          </div>
-          <div className="flex flex-col items-center justify-center">
-            <label className="text-2xl font-bold">Preparation</label>
-            <input></input>
-          </div>
-          <div className="flex flex-col items-center justify-center">
-            <label className="text-2xl font-bold">Advises</label>
-            <input></input>
-          </div>
-          <div className="flex flex-col items-center justify-center">
-            <label className="text-2xl font-bold">Image</label>
-            <input></input>
-          </div>
-          <div className="flex flex-col items-center justify-center">
-            <label className="text-2xl font-bold">Category</label>
-            <input></input>
+          <div className="border-t-4 border-black mt-5">
+            <div className="flex flex-col items-center justify-center">
+              <label className="text-2xl font-bold">Preparation</label>
+              <div className="flex flex-row items-center justify-center">
+                <input className="w-12 m-2"></input>
+                <input className="w-12 m-2"></input>
+                <input className="w-12 m-2"></input>
+              </div>
+              <div className="flex flex-row items-center justify-center">
+                <input className="w-12 m-2"></input>
+                <input className="w-12 m-2"></input>
+                <input className="w-12 m-2"></input>
+              </div>
+            </div>
           </div>
         </form>
       </div>
